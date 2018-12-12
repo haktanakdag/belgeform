b8fec1f3fa61cd466085f9bf7fb8319ccc38288e
Haktan <haktan.akdag@gmail.com>
2018-12-12 15:14:18 +0300

    Add files via upload

---
+++ jqislem.php
@@ -0,0 +1,40 @@
+﻿<?php
+error_reporting(E_ALL ^ E_NOTICE); 
+ob_start();
+session_start();
+extract($_GET);
+extract($_POST);
+ob_flush();
+
+include "dbclass.php";
+include "cls_stok.php";
+
+if($jqstokcek=="var"){
+    echo $stokid;
+    $list = new Stok();
+    $dlist =$list->StokGetir($stokid);
+    ob_clean();
+    echo $dlist->stokkod ." - ". $dlist->stokad;
+}
+if($jqstokbirimcek=="var"){
+    echo $stokid;
+    $list = new Stok();
+    $dlist =$list->StokGetir($stokid);
+    ob_clean();
+    echo $dlist->birim;
+}
+if($jqstokfiyatcek=="var"){
+    echo $stokid;
+    $list = new Stok();
+    $dlist =$list->StokGetir($stokid);
+    ob_clean();
+    echo $dlist->birim;
+}
+if($jqstokkdvcek=="var"){
+    echo $stokid;
+    $list = new Stok();
+    $dlist =$list->StokGetir($stokid);
+    ob_clean();
+    echo $dlist->kdvoran;
+}
+?>
